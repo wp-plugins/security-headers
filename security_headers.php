2898ec6fd4e3b266c81152d0c53546c7d59c32a6
SimonRWaters <SimonRWaters@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 11:22:40 +0000

    Version number in code comments fixed
    git-svn-id: https://plugins.svn.wordpress.org/security-headers/trunk@1136094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- security_headers.php
+++ security_headers.php
@@ -3,7 +3,7 @@
  * Plugin Name: HTTP Headers
  * Plugin URI: http://surevine.com/
  * Description: Sets security related headers (HSTS etc)
- * Version: 0.2
+ * Version: 0.4
  * Author: Simon Waters (Surevine Ltd)
  * Author URI: http://waters.me/
  * License: GPL2 or any later version
